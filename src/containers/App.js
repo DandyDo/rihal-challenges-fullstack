95d97c1bcf7af33063171e919b544896c3ac6766
DandyDo <alqasimtoon@gmail.com>
2022-01-11 02:38:37 +0400

    updated the mock database

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,9 +2,21 @@ import './App.css';
 import Classes from '../components/Classes';
 
 function App() {
+  const requestStudents = () => {
+    fetch('http://localhost:3000/students')
+      .then(response => response.json())
+      .then(students => console.log(students))
+      .catch(err => console.log(err));
+  }
+  requestStudents();
+
+  // const studentsPerClass = () => {
+  //   
+  // }
+
   return (
     <div className="App">
-      <Classes/>
+      <Classes />
     </div>
   );
 }
