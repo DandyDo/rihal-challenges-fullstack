861b66aee88584662ee7b9dbd27cbbd6c18a4c19
DandyDo <alqasimtoon@gmail.com>
2022-01-11 12:16:36 +0400

    added adding hooks and some data manipulations

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,38 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import Classes from '../components/Classes';
 
 function App() {
-  const requestStudents = () => {
+  const [students, setStudents] = useState([])
+  const [classes, setClasses] = useState({})
+  const [countries, setCountries] = useState({})
+
+  // Get students from local db.json (using json-server)
+  useEffect(() => {
     fetch('http://localhost:3000/students')
       .then(response => response.json())
-      .then(students => console.log(students))
+      .then(data => {setStudents(data)})
       .catch(err => console.log(err));
-  }
-  requestStudents();
+  }, [])
 
-  // const studentsPerClass = () => {
-  //   
-  // }
+  // Get the classes/countries IDs only from students
+  const countryOfStudents = students.map(({ country_id }) => (country_id));
+  const classOfStudents = students.map(({ class_id }) => (class_id));
+
+  // Group the classes/countries IDs
+  const studentsPerCountry = countryOfStudents.reduce((acc, b) => {
+    acc[b] = acc[b] + 1 || 1
+    return acc;
+  }, {});
+  const studentsPerClass = classOfStudents.reduce((acc, b) => {
+    acc[b] = acc[b] + 1 || 1
+    return acc;
+  }, {});
+
+  console.log(countryOfStudents);
+  console.log(studentsPerCountry);
+  console.log(classOfStudents);
+  console.log(studentsPerClass);
 
   return (
     <div className="App">
