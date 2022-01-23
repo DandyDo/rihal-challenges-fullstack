80182392cfe80be647a1f2308da3ae8c62106c5e
DandyDo <alqasimtoon@gmail.com>
2022-01-24 00:29:57 +0400

    added inputs, buttons, and styles to new components. TODO: create the final CRUD (update and delete) components and then begin on implementing CRUD and behaviors.

--- src/components/Lists/Classes.js
+++ src/components/Lists/Classes.js
@@ -2,26 +2,35 @@ import React from 'react';
 
 const Classes = ({ classes }) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Class Name</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          classes.map(course => {
-            return (
-              <tr key={course.id}>
-                <td>{course.id}</td>
-                <td>{course.class_name}</td>
-              </tr>
-            );
-          })
-        }        
-      </tbody>
-    </table>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Class Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            classes.map(course => {
+              return (
+                <tr key={course.id}>
+                  <td>{course.id}</td>
+                  <td>{course.class_name}</td>
+                </tr>
+              );
+            })
+          }        
+        </tbody>
+      </table>
+      <div className='pb3 mv4 ba br3 bw1 w-60 center'>
+        <h3>Click on a row to modify/delete. Or add a class below:</h3>
+        <label htmlFor='class_name'>Class name: </label>
+        <input className='ma2' placeholder='Enter class' type='text' id='class_name' name='class_name'/>
+        <button className='mt3 w-20'>Add</button>
+      </div>
+    </div>
+
   );
 }
 
--- src/components/Lists/Countries.js
+++ src/components/Lists/Countries.js
@@ -2,26 +2,34 @@ import React from 'react';
 
 const Countries = ({ countries }) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Country</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          countries.map(country => {
-            return (
-              <tr key={country.id}>
-                <td>{country.id}</td>
-                <td>{country.country_name}</td>
-              </tr>
-            );
-          })
-        }        
-      </tbody>
-    </table>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Country</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            countries.map(country => {
+              return (
+                <tr key={country.id}>
+                  <td>{country.id}</td>
+                  <td>{country.country_name}</td>
+                </tr>
+              );
+            })
+          }        
+        </tbody>
+      </table>   
+      <div className='pb3 mv4 ba br3 bw1 w-60 center'>
+        <h3>Click on a row to modify/delete. Or add a country below:</h3>
+        <label htmlFor='country'>Country: </label>
+        <input className='ma2' placeholder='Enter country' type='text' id='country' name='country'/>
+        <button className='mt3 w-20'>Add</button>
+      </div>   
+    </div>
   );
 }
 
