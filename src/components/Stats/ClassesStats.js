90fe64fec37b12b46c9748a803cd6a99e6cd62e9
dandydo <alqasimtoon@gmail.com>
2022-01-30 03:28:55 +0400

    needs revision. Temp 'fix'.

--- src/components/Stats/ClassesStats.js
+++ src/components/Stats/ClassesStats.js
@@ -2,17 +2,19 @@ import React from "react";
 
 const ClassesStats = ({ classes, students }) => {
     // Group the object's key values (for example in case of class_id, 1: 6, 2: 3, 3: 12, ...)
+    // then returns an array of values from the object
     const groupObjKeyValue = (studentsKey) => {
         const result = studentsKey.reduce((acc, b) => {
         acc[b] = acc[b] + 1 || 1
         return acc;
         }, {});
 
-        return result;
+        return Object.values(result);
     }
 
     // Extract the classes IDs only from students
     const studentsClassesKey = students.map(({ class_id }) => (class_id));
+    // Get returns an array of values from the object.
     const studentsPerClass = groupObjKeyValue(studentsClassesKey);
 
     return (
@@ -21,7 +23,7 @@ const ClassesStats = ({ classes, students }) => {
             {
                 classes.map(({ class_name }, i) => {                        
                     return (
-                        <div key={i}>{`${class_name}: ${studentsPerClass[i+1] ? studentsPerClass[i+1] : 0}`}</div>
+                        <div key={i}>{`${class_name}: ${studentsPerClass[i] ? studentsPerClass[i] : 0}`}</div>
                     );
                 })
             }
--- src/components/Stats/CountriesStats.js
+++ src/components/Stats/CountriesStats.js
@@ -2,16 +2,19 @@ import React from "react";
 
 const CountriesStats = ({ countries, students }) => {
     // Group the object's key values (for example in case of country_id, 1: 6, 2: 3, 3: 12, ...)
+    // then returns an array of values from the object
     const groupObjKeyValue = (studentsKey) => {
         const result = studentsKey.reduce((acc, b) => {
         acc[b] = acc[b] + 1 || 1
         return acc;
         }, {});
 
-        return result;
+        return Object.values(result);
     }
+    
     // Extract the countries IDs only from students
     const studentsCountryKey = students.map(({ country_id }) => (country_id));
+    // Get returns an array of values from the object.
     const studentsPerCountry = groupObjKeyValue(studentsCountryKey);
 
     return (
@@ -20,7 +23,7 @@ const CountriesStats = ({ countries, students }) => {
             {
                 countries.map(({ country_name }, i) => {                        
                     return (
-                        <p key={i}>{`${country_name}: ${studentsPerCountry[i+1] ? studentsPerCountry[i+1] : 0}`}</p>
+                        <p key={i}>{`${country_name}: ${studentsPerCountry[i] ? studentsPerCountry[i] : 0}`}</p>
                     );
                 })
             }
